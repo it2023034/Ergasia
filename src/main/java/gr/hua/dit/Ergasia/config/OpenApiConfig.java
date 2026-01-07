c1ca405752bf12b78cbabbfe16655b341da6b22e
it2023034 <it2023034@hua.gr>
2026-01-07 15:57:20 +0200

    Initial commit

---
+++ src/main/java/gr/hua/dit/Ergasia/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package gr.hua.dit.Ergasia.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI (Swagger) configuration for Ergasia REST API.
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Ergasia REST API")
+                        .version("v1")
+                        .description("REST API for Ergasia project")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .packagesToScan("gr.hua.dit.Ergasia.web.rest")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+}
